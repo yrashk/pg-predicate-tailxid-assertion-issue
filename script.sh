b5f3520f3f98ad3faffa3912a6e4b96814c2bad8
Yurii Rashkovskii <yrashk@gmail.com>
2024-10-16 08:17:45 -0700

    Simplify test provisioning with a shell script.
    It will [re]-install Rust locally.
    
    It will also launch Postgres (you'd need to stop it)

---
+++ script.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+PORT=65432
+
+if [[ ! -f ".ready" ]]; then
+  # Install Rust
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-update-default-toolchain
+  . "$HOME/.cargo/env"
+
+  # Postgres
+  if [[ ! -d postgres ]]; then
+    git clone https://github.com/postgres/postgres --branch=REL_17_STABLE --depth=1
+  fi
+  cd postgres
+  ./configure --prefix=$(pwd)/build --without-icu --enable-cassert --without-readline
+  make -j all && make install
+  cd ..
+  if [[ ! -d db ]]; then
+    postgres/build/bin/initdb -D db -c port=$PORT
+  fi
+  if [[ -f db/postmaster.pid ]]; then
+    postgres/build/bin/pg_ctl -D db -l logfile stop
+  fi
+  postgres/build/bin/pg_ctl -D db -l logfile start
+  postgres/build/bin/createdb -e -p $PORT test || true
+  touch .ready
+fi
+
+PG_URL=postgres://localhost:$PORT/test cargo run --release
